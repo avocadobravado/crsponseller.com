cfac827fd90ce743798e2172e2d63be3d465c5b5
Schazzwozzer <cory@crsponseller.com>
2017-11-01 15:21:44 -0700

    Parameterize halftone colors
    Halftone shader colors are now fetched from the canvas' style.
    'background-color' and 'color' specifically.

--- js/shaders-halftone.js
+++ js/shaders-halftone.js
@@ -49,7 +49,7 @@ var fShader = `
 		//float xOffset = mod( u_resolution.x, u_dotSize ) / 2.0;
 		//float yOffset = mod( u_resolution.y, u_dotSize ) / 2.0;
 	
-		// Calculate distance on both axes to a range of -1.0 — 1.0.
+		// Calculate distance on both axes to a range of -1.0 ï¿½ 1.0.
 		float distanceX = mod(pixelPosition.x, u_dotSize) / u_dotSize * 2.0 - 1.0;
 		float distanceY = mod(pixelPosition.y, u_dotSize) / u_dotSize * 2.0 - 1.0;
 		// 0.7 is a stand-in for half the square root of 2, which 
@@ -131,6 +131,7 @@ var fShader = `
 // The rest of this file is pretty directly 
 // taken from the TWGL "tiniest example".
 const gl = document.getElementById( "canvas" ).getContext( "webgl" );
+var style = window.getComputedStyle( document.getElementById( "canvas" ));
 const programInfo = twgl.createProgramInfo( gl, [vShader, fShader] );
 
 // 3d vectors for six positions (composing a single quad)
@@ -138,6 +139,18 @@ const arrays = {
 	position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
 };
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+
+// Convert an RGB(A) value (from CSS) to an array of scalar values.
+function extractColor( cssValue )
+{
+	// Note that no hex conversion is necessary.
+	// CSSStyleDeclaration already provides colors as RGB(A) strings.
+
+	// This line extracts just the number values
+	// via https://stackoverflow.com/a/1183906
+	var array = cssValue.match(/\d+/g);
+	return [array[0]/255, array[1]/255, array[2]/255, 1.0];
+}
 
 function render(time)
 {
@@ -163,8 +176,8 @@ function render(time)
 		u_gradientAngle: 0.0,
 		// Scalar value determine 
 		u_gradientWidth: 0.333333,
-		bgColor:     [0.5, 1.0, 0.75, 1.0],
-		dotColor:    [0.5, 0.5, 1.0, 1.0]
+		bgColor: extractColor( style.getPropertyValue('background-color')),
+		dotColor: extractColor( style.getPropertyValue('color')),
 	};
 
 	gl.useProgram(programInfo.program);
